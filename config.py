5a68b4384121052f860cd43e5cb404afa67d4637
Isleyter <151705437+Isleyter@users.noreply.github.com>
2025-05-25 15:11:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,14 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     MONGODB_SETTINGS = {
-        'db': 'data',
-        'host': 'localhost',
-        'port': 27017
+        'host': os.environ.get('MONGODB_URI')
     }
+    
+#class Config:
+ #   SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+  #  MONGODB_SETTINGS = {
+   #     'db': 'data',
+    #    'host': 'localhost',
+     #   'port': 27017
+    #}
+    
