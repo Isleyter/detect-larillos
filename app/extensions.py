2c6e59f5a63fa4896d475f76ddb8cd6954ae9a09
Isleyter <151705437+Isleyter@users.noreply.github.com>
2025-05-28 02:36:48 -0500

    Delete app directory

--- app/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_mongoengine import MongoEngine # type: ignore
-from flask_login import LoginManager # type: ignore
-from flask_bcrypt import Bcrypt # type: ignore
-
-
-db = MongoEngine()
-login_manager = LoginManager()
-bcrypt = Bcrypt()
-#migrate = Migrate()  # puedes omitirlo si no usas migraciones
